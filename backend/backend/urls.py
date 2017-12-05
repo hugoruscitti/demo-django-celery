3b982b219dd6c9780f0227020c3c76ed54217a0e
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-12-04 22:02:45 -0300

    Agreagando celery y algunas tareas.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,5 +5,6 @@ from tareas import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('demo/', views.demo, name='demo'),
+    path('crearTarea/', views.crearTarea, name='crearTarea'),
     path('admin/', admin.site.urls),
 ]
--- backend/tareas/views.py
+++ backend/tareas/views.py
@@ -1,8 +1,22 @@
 from django.http import HttpResponse
+from . import tasks
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("""
+Rutas principales
+
+<ul>
+    <li><a href="/crearTarea">Crear tarea</a></li>
+    <li><a href="/demo">Demo</a></li>
+</ul>
+
+
+""")
 
 
 def demo(request):
     return HttpResponse("Demo!")
+
+def crearTarea(request):
+    tarea = tasks.add.delay(2, 3)
+    return HttpResponse("Creando tarea" + str(tarea))
