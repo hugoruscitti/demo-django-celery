55f7330d3c990dbb33f5a64e07019ec8b6f3b8b4
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-12-04 22:29:33 -0300

    Agregando archivos para especificar locación de archivos estáticos y correr las migraciones.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
